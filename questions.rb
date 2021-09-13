d25f7c3155c8fe22360a00ccbe874972f2b96a9f
Anthony Kim <fbssaejun@gmail.com>
2021-09-13 16:15:37 +0000

    add answer variable

--- questions.rb
+++ questions.rb
@@ -5,6 +5,7 @@ class Question
     randomNumber2 = rand(1..20)
    
     @question =  "What does #{randomNumber1} plus #{randomNumber2} equal?"
+    @answer = randomNumber1 + randomNumber2
   end
 
 end
